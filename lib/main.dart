96b8aa9c9051faeab90c07a357982ec14a078291
marcoslora <marcoslora95@gmail.com>
2023-07-14 11:40:16 -0400

    delete imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:widgets_app/presentation/screen/cards/cards_screen.dart';
-import 'package:widgets_app/presentation/screen/buttons/buttons_screen.dart';
-import 'package:widgets_app/presentation/screen/home/home_screen.dart';
 
 import 'config/theme/app_theme.dart';
 import 'config/theme/router/app_router.dart';
